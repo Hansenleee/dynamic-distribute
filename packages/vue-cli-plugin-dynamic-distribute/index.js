13d3723569c6c65aab263b2fddbeaad662d19684
libin_li <libin_li@youzan.com>
2022-04-27 14:39:57 +0800

    fix: 修改 vue-cli 编译时配置

--- packages/vue-cli-plugin-dynamic-distribute/index.js
+++ packages/vue-cli-plugin-dynamic-distribute/index.js
@@ -4,7 +4,7 @@ const dynamic_distribute_build_1 = require("dynamic-distribute-build");
 module.exports = (api, options) => {
     const builder = new dynamic_distribute_build_1.WebpackChainBuild({
         chain: api.chainWebpack.bind(api),
-        config: (options.pluginOptions || {}).dynamicDistribute,
+        config: (options.pluginOptions || {}).dynamicDistribute || {},
     });
     builder.configWebpackchain();
 };
