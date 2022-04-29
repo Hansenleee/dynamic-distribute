6c83e664df56d97ec98d9d0c9d77626e6ac1e15f
libin_li <libin_li@youzan.com>
2022-04-29 10:58:51 +0800

    fix: 修改 vue-cli 打包配置

--- packages/vue-cli-plugin-dynamic-distribute/index.ts
+++ packages/vue-cli-plugin-dynamic-distribute/index.ts
@@ -2,10 +2,12 @@ import { WebpackChainBuild, IDynamicPluginConfig } from 'dynamic-distribute-buil
 import type { PluginAPI, ProjectOptions } from '@vue/cli-service';
 
 module.exports = (api: PluginAPI, options: ProjectOptions) => {
-  const builder = new WebpackChainBuild({
-    chain: api.chainWebpack.bind(api),
-    config: ((options.pluginOptions || {}) as { dynamicDistribute: IDynamicPluginConfig }).dynamicDistribute || {},
-  });
-
-  builder.configWebpackchain();
+  if (process.env.DYNAMIC_DISTRIBUTE) {
+    const builder = new WebpackChainBuild({
+      chain: api.chainWebpack.bind(api),
+      config: ((options.pluginOptions || {}) as { dynamicDistribute: IDynamicPluginConfig }).dynamicDistribute || {},
+    });
+  
+    builder.configWebpackchain();
+  }
 }
